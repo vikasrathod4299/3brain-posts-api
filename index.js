775c4dfea22eadd93a878615aed2374a7e8d1c54
vikasrathod4299 <vikas.rv4299@gmail.com>
2023-09-11 15:48:21 +0530

    fix : handled case where uploads directory dose not exists

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const multer = require("multer");
+const fs = require("fs");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
@@ -15,6 +15,14 @@ app.use(bodyParser.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
+
+if (!fs.existsSync("uploads")) {
+  try {
+    fs.mkdirSync("uploads", { recursive: true });
+  } catch (err) {
+    console.log("Somthing went wrong while createing uploads directory!");
+  }
+}
 
 mongoose
   .connect(process.env.DB_URL)
